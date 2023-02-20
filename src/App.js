9557f9d3e2fd811c0f9494f95632b6abe42d1b4f
João Victor <joao.victor.valentim@hotmail.com>
2023-02-20 19:05:48 -0300

    Add comentarios

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+// Importando as bibliotecas e módulos necessários
 import React, { useState } from 'react';
 import moment from 'moment';
 import 'moment/locale/pt-br';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  // Configurando o idioma do Moment.js
   moment.locale('pt-br');
 
+  // Definindo os estados iniciais
   const [numDays, setNumDays] = useState(0);
   const [numHours, setNumHours] = useState(0);
   const [startDate, setStartDate] = useState(moment());
   const [medicationSchedule, setMedicationSchedule] = useState([]);
 
+  // Função para criar a lista de horários da medicação
   const setSchedule = (days, frequency) => {
+    // Verificando se a data de início selecionada é válida
     if (!startDate.isValid()) {
       alert('Por favor, selecione uma data de início válida');
       return;
     }
 
+    // Criando a lista de horários da medicação
     const scheduleList = [];
     let dateTime = moment(startDate);
 
@@ -26,77 +32,80 @@ function App() {
       }
       dateTime.add(1, 'day');
     }
-    scheduleList.pop()
-    setMedicationSchedule(scheduleList);
-  }
 
-  return (
-    <div className="container mt-5">
-      <div className="card p-5">
-        <h1 className="mb-4">Calculadora de Horários de Medicamentos</h1>
-        <div className="row mb-3">
-          <label className="col-sm-2 col-form-label">Quantidade de dias:</label>
-          <div className="col-sm-2">
-            <input
-              className="form-control"
-              type="number"
-              value={numDays}
-              onChange={(event) => setNumDays(event.target.value)}
-              min="0"
-            />
+    // Removendo o último item da lista (que pode ser desnecessário)
+    scheduleList.pop()
+
+    // Armazenando a lista de horários no estado medicationSchedule
+    setMedicationSchedule(scheduleList);
+ }
+    return (
+      <div className="container mt-5">
+        <div className="card p-5">
+          <h1 className="mb-4">Calculadora de Horários de Medicamentos</h1>
+          <div className="row mb-3">
+            <label className="col-sm-2 col-form-label">Quantidade de dias:</label>
+            <div className="col-sm-2">
+              <input
+                className="form-control"
+                type="number"
+                value={numDays}
+                onChange={(event) => setNumDays(event.target.value)}
+                min="0"
+              />
+            </div>
           </div>
-        </div>
-        <div className="row mb-3">
-          <label className="col-sm-2 col-form-label">De quantas em quantas horas:</label>
-          <div className="col-sm-2">
-            <input
-              className="form-control"
-              type="number"
-              value={numHours}
-              onChange={(event) => setNumHours(event.target.value)}
-              min="0"
-            />
+          <div className="row mb-3">
+            <label className="col-sm-2 col-form-label">De quantas em quantas horas:</label>
+            <div className="col-sm-2">
+              <input
+                className="form-control"
+                type="number"
+                value={numHours}
+                onChange={(event) => setNumHours(event.target.value)}
+                min="0"
+              />
+            </div>
           </div>
-        </div>
-        <div className="row mb-3">
-          <label className="col-sm-2 col-form-label">Data de início:</label>
-          <div className="col-sm-4">
-            <input
-              className="form-control"
-              type="datetime-local"
-              value={startDate.format('YYYY-MM-DDTHH:mm')}
-              onChange={(event) => setStartDate(moment(event.target.value, "YYYY-MM-DDTHH:mm"))}
-            />
+          <div className="row mb-3">
+            <label className="col-sm-2 col-form-label">Data de início:</label>
+            <div className="col-sm-4">
+              <input
+                className="form-control"
+                type="datetime-local"
+                value={startDate.format('YYYY-MM-DDTHH:mm')}
+                onChange={(event) => setStartDate(moment(event.target.value, "YYYY-MM-DDTHH:mm"))}
+              />
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-2"></div>
-          <div className="col-sm-4">
-            <button
-              className="btn btn-primary"
-              onClick={() => setSchedule(numDays, numHours)}
-            >
-              Calcular Horários
-            </button>
+          <div className="row">
+            <div className="col-sm-2"></div>
+            <div className="col-sm-4">
+              <button
+                className="btn btn-primary"
+                onClick={() => setSchedule(numDays, numHours)}
+              >
+                Calcular Horários
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="row mt-5">
-          <div className="col">
-            <h3>Horários de Medicação:</h3>
-            {medicationSchedule.length === 0 ? (
-              <p>Nenhum horário calculado.</p>
-            ) : (
-              <ul className="list-group">
-                {medicationSchedule.map((scheduleItem, index) => (
-                  <li className="list-group-item" key={index}>{scheduleItem}</li>
-                ))}
-              </ul>
-            )}
+          <div className="row mt-5">
+            <div className="col">
+              <h3>Horários de Medicação:</h3>
+              {medicationSchedule.length === 0 ? (
+                <p>Nenhum horário calculado.</p>
+              ) : (
+                <ul className="list-group">
+                  {medicationSchedule.map((scheduleItem, index) => (
+                    <li className="list-group-item" key={index}>{scheduleItem}</li>
+                  ))}
+                </ul>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default App;
+  export default App;
