3c5f0e1fe0dd5b5c21a0adfbdd5602e69853b460
João Victor <joao.victor.valentim@hotmail.com>
2023-02-20 18:19:42 -0300

    Calcular Horario Medicamentos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import moment from 'moment';
+import 'moment/locale/pt-br'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  moment.locale('pt-br');
+
+  const [numDays, setNumDays] = useState(0);
+  const [numHours, setNumHours] = useState(0);
+  const [startDate, setStartDate] = useState(moment());
+  const [medicationSchedule, setMedicationSchedule] = useState([]);
+
+  const setSchedule = (days, frequency) => {
+    if (!startDate.isValid()) {
+      alert('Por favor, selecione uma data de início válida');
+      return;
+    }
+  
+    const scheduleList = [];
+    let dateTime = moment(startDate);
+
+    for (let i = 0; i < days; i++) {
+      for (let j = 0; j < 24 / frequency; j++) {
+        scheduleList.push(dateTime.format('dddd, D [de] MMMM [às] HH:mm'));
+        dateTime.add(frequency, 'hours');
+      }
+      dateTime.add(1, 'day');
+    }
+  
+    setMedicationSchedule(scheduleList);
+  }
+
+  const calculateMedicationSchedule = () => {
+    const scheduleList = [];
+    let currentDate = moment(startDate);
+    let numDoses = Math.floor(numDays * 24 / numHours);
+    let interval = numHours / numDoses;
+
+    for (let i = 0; i < numDoses; i++) {
+      scheduleList.push(currentDate.format('dddd, D [de] MMMM [às] HH:mm'));
+      currentDate.add(interval, 'hours');
+    }
+
+    return scheduleList;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="mb-4">Calculadora de Horários de Medicamentos</h1>
+      <div className="row mb-3">
+        <label className="col-sm-2 col-form-label">Quantidade de dias:</label>
+        <div className="col-sm-2">
+          <input
+            className="form-control"
+            type="number"
+            value={numDays}
+            onChange={(event) => setNumDays(event.target.value)}
+          />
+        </div>
+      </div>
+      <div className="row mb-3">
+        <label className="col-sm-2 col-form-label">De quantas em quantas horas:</label>
+        <div className="col-sm-2">
+          <input
+            className="form-control"
+            type="number"
+            value={numHours}
+            onChange={(event) => setNumHours(event.target.value)}
+          />
+        </div>
+      </div>
+      <div className="row mb-3">
+        <label className="col-sm-2 col-form-label">Data de início:</label>
+        <div className="col-sm-4">
+          <input
+            className="form-control"
+            type="datetime-local"
+            value={startDate.format('YYYY-MM-DDTHH:mm')}
+            onChange={(event) => setStartDate(moment(event.target.value, "YYYY-MM-DDTHH:mm"))}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-2"></div>
+        <div className="col-sm-4">
+          <button
+            className="btn btn-primary"
+            onClick={() => setSchedule(numDays, numHours)}
+          >
+            Calcular Horários
+            </button>
+        </div>
+      </div>
+      <div className="row mt-5">
+        <div className="col">
+          <h3>Horários de Medicação:</h3>
+          <ul>
+            {medicationSchedule.map((scheduleItem, index) => (
+              <li key={index}>{scheduleItem}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
